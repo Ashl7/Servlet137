e7aa7e66464048994b20d1c3f30b0169aa82eb48
Anahit <anahit@192.168.1.111>
2016-05-31 03:43:35 -0700

    add to cart button and logic that adds item to session cart

---
+++ src/java/items/AddToCart.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package items;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author anahit
+ */
+public class AddToCart extends HttpServlet{
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        int id = Integer.parseInt(request.getParameter("clicked_button"));
+        
+        HttpSession session = request.getSession(false);
+        ShoppingCart cart = (ShoppingCart) session.getAttribute("ShoppingCart");
+        if (cart == null)
+        {
+            cart = new ShoppingCart();
+
+            session.setAttribute("ShoppingCart", cart);
+        }
+
+        cart.addItem(id, 1);
+    }
+    
+}
